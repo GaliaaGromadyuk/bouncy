c5d7dfcf8b081481335cf4f0dc3f027f5552fede
Galia Gromadyuk <byhalen1@gmail.com>
2017-11-26 20:04:30 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+$('.ba-team__slider').slick({
+  dots: true,
+  infinite: true,
+  arrows: false,
+  speed: 300,
+  slidesToShow: 1,
+  slide: ".ba-team__slider-item"
+});
+
+$('.ba-testimonials__slider').slick({
+  dots: true,
+  arrows: false,
+  infinite: true,
+  speed: 300,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  slidesToShow: 1,
+  slide: ".ba-testimonials__slider-item"
+});
+
+
